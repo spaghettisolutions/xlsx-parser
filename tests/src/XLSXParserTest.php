027268ddf05708d4eb56427577dfa8aa8db675d0
Dāvis Zālītis <davis.zalitis@whitedigital.eu>
2024-03-18 15:48:16 +0200

    chore: add test for data validity

--- tests/src/XLSXParserTest.php
+++ tests/src/XLSXParserTest.php
@@ -27,6 +27,28 @@ class XLSXParserTest extends TestCase
         $this->assertEquals(expected: new DateTimeImmutable(datetime: '2022-12-05'), actual: $values[2][5]);
     }
 
+    public function testDataValidity(): void
+    {
+        $workbook = (new XLSXParser())->open(path: dirname(path: __DIR__) . '/assets/mock.xlsx');
+        $this->assertEquals(expected: ['data', ], actual: $workbook->getWorksheets());
+        $values = [];
+        foreach ($workbook->getRows(index: $workbook->getIndex(name: 'data')) as $key => $row) {
+            $values[$key] = $row;
+        }
+
+        $this->assertCount(expectedCount: 1001, haystack: $values);
+        $data = [
+            'JM1NC2MF8E0960570', 'Acura', 'TSX', 2012, 'Teal', 218514, 'Electric', 'Manual', 5.6, 272, 159, 1,
+            73103.86, '5/10/2022', 'Augusto Grissett', 27, 'Male', '85426 International Trail', 'Néa Karyá',
+            'Greece', '4/24/2018', '', 'XYZ789', 'Topicblab', 469689, '3/7/2029', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+
+Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', true, 'Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', false,
+        ];
+        $this->assertEquals(expected: $data, actual: $values[2]);
+    }
+
     public function testOpenCellXfs(): void
     {
         $workbook = (new XLSXParser())->open(path: dirname(path: __DIR__) . '/assets/samplefile.xlsx');
